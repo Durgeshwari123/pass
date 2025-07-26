efc865ddb991f89a1daa18bcaa354a75e557d55f
Durgeshwari123 <115608465+Durgeshwari123@users.noreply.github.com>
2025-07-26 16:04:59 +0530

    Add files via upload

---
+++ curl.sh
@@ -0,0 +1,3 @@
+curl -X POST -H "Content-Type: application/json" \
+-d '{"text":"Deployment completed successfully ✅"}' \
+https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX
